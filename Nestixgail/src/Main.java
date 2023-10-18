fd83c090e64bdcd15944c8626ffc7c90c483a19c
Aleksey <aleksejbaskirov31@gmail.com>
2023-10-18 23:14:27 +0300

    Game Matches 2 Lesson

--- Nestixgail/src/Main.java
+++ Nestixgail/src/Main.java
@@ -17,33 +17,72 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             // System.out.println("i = " + i);
 //        }
+        final int MINIMUM_PLAYER = 2;
+        final int MAXIMUM_PLAYER = 16;
+        final int MINIMUM_MATCHES = 5;
+        final int MAXIMUM_MATCHES = 100;
+        final int MINIMUM_MATCHES_PER_ROUND = 1;
+        final int MAXIMUM_MATCHES_PER_ROUND = 3;
         int countMatches, countPlayers, nowRound = 1;
         int countMatchesOut, nowTurn = 1;
         boolean gameEnd = false;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите числщ Игроков:");
-        countPlayers = scanner.nextInt();
-        System.out.print("Введите количество спичек:");
-        countMatches = scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите числщ Игроков:");
+//        countPlayers = scanner.nextInt();
+        countPlayers = inputInt("Введите числщ Игроков: ", MINIMUM_PLAYER, MAXIMUM_PLAYER);
+//        System.out.print("Введите количество спичек:");
+//        countMatches = scanner.nextInt();
+        System.out.println("Приветствуем Вас в игре \"Спички\" !");
+        System.out.println("Количество Игроков От " + MINIMUM_PLAYER + " До " + MAXIMUM_PLAYER);
+        System.out.println("Каждый игрок По очереди Вытягивает От " + MINIMUM_MATCHES_PER_ROUND + " До " +
+                MAXIMUM_MATCHES_PER_ROUND + " Спичек.");
+        countMatches = inputInt("Введите Количество Спичек: ", MINIMUM_MATCHES,
+                MAXIMUM_MATCHES);
         while (!gameEnd) {
-            System.out.println("Раунд: №" + nowRound + " !\n"
+            System.out.println("Раунд: № " + nowRound++ + " !\n"
             + "Сейчас На Столе " + countMatches + " Спичек.") ;
             for (nowTurn =1; nowTurn <= countPlayers; nowTurn++) {
-                System.out.print("Введите количество Спичек" +
-                        "Ход Игрока № "+ nowTurn
-                        + " которое Вы Хотите Убрать!");
-                countMatchesOut = scanner.nextInt();
+//                System.out.print("Введите количество Спичек" +
+//                        "Ход Игрока № "+ nowTurn
+//                        + " которое Вы Хотите Убрать!");
+//                System.out.print("Ход Игрока № " + nowTurn);
+//                countMatchesOut = scanner.nextInt();
+                countMatchesOut = inputInt("Ход Игрока № " + nowTurn +
+                        " Введите Количество Спичек, " +
+                        "которое Вы бы Хотели Убрать ", MINIMUM_MATCHES_PER_ROUND, MAXIMUM_MATCHES_PER_ROUND);
                 countMatches = countMatches - countMatchesOut;
                 if (countMatches <= 0){
                     gameEnd = true;
                     break;
                 }
             }
-            System.out.println("Игра закончена! " +
-                    "Последнюю спичку вытащил Игрок №"
-            + nowTurn);
+//            nowRound++;
         }
+        System.out.println("Игра закончена! " +
+                "Последнюю спичку вытащил Игрок № "
+                + nowTurn);
 
 //        String next = scanner.next();
+    }
+
+    public static int inputInt(String msg, int min, int max) {
+        System.out.print(msg);
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+            if (number < min || number > max) {
+                System.out.println("Введенное число должно быть " +
+                        "в диапазоне от " + min + " до " + max);
+                System.out.println("Введите число ещё раз.");
+                number = inputInt(msg, min, max);
+            }
+        }
+        else {
+            System.out.println("Вы Ввели НЕ Целое Число !");
+            System.out.println("Введите Целое Число: ");
+            number = inputInt(msg, min, max);
+        }
+        return number;
     }
 }
